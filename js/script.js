d2b07ecdb551068f1bdc6ecd3eb626fe9575d1b1
Giulia <giuliatullia93@gmail.com>
2021-06-17 00:43:51 +0200

    add-snack1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,57 @@
 // Snack 1 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+
+
+// const ar = [
+//     {
+//         nome : 'bici1',
+//         peso : 8
+//     },
+//     {
+//         nome : 'bici2',
+//         peso : 12
+//     },
+//     {
+//         nome : 'bici3',
+//         peso : 16
+//     },
+//     {
+//         nome : 'bici4',
+//         peso : 9
+//     }
+
+// ];
+
+// let biciLeggera = ar[0];
+
+// for(let i = 0; i < ar.length; i++){
+//     if(ar[i].peso < biciLeggera.peso){
+//         biciLeggera = ar[i];
+//     }
+// }
+
+// // console.log(biciLeggera);
+
+// const {nome, peso} = biciLeggera;
+// console.log(nome, peso);
+
+// document.getElementById('container').innerHTML = 
+// `
+// <ul>
+//  <li>Nome: ${nome}</li>
+//  <li>Peso: ${peso}</li>
+// </ul>
+// `
+// ;
+
+
+
+
+
+
+
+
+
+
+
 // Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (Utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti. Stampiamo tutto in console.
